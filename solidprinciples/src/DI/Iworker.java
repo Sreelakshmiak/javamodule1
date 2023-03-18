0fcf2e313141f48f82703769cdf707f15384a206
Sreelakshmiak <245277@ust.com>
2023-03-18 09:42:39 +0530

    program

--- solidprinciples/src/DI/Iworker.java
+++ solidprinciples/src/DI/Iworker.java
@@ -1,5 +1,28 @@
 package DI;
 
 public interface Iworker {
+	public void work();
+}
 
+class Worker1 implements Iworker{
+	public void work() {
+		
+	}
+}
+class SuperWorker implements Iworker{
+	public void work() {
+		//working much more
+	}
+}
+
+class Manager1 {
+	Iworker worker;
+	public void setWorker(Iworker w)
+	{
+		worker=w;
+		
+	}
+	public void manger() {
+		worker.work();
+	}
 }
--- solidprinciples/src/DI/Worker.java
+++ solidprinciples/src/DI/Worker.java
@@ -2,4 +2,26 @@ package DI;
 
 public class Worker {
 
+public void work() {
+	//;..working
+}
+
+class Manager{
+	Worker worker;
+	public void setWirker(Worker w) {
+		worker=w;
+	}
+	public void manage() {
+		worker.work();
+		
+	}
+}
+
+class superWorker{
+	public void work()
+	{
+		//.. working much more
+	}
+}
+
 }
